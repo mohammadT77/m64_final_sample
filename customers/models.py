2970a439e6a6eb70f3d1dee9e9fd2e768947d284
mahdi-kh <mehdikhatir1375@gmail.com>
2022-02-18 16:10:28 +0330

    backend | customers | models.py | Complete Address & Customer Model

--- customers/models.py
+++ customers/models.py
@@ -1,4 +1,14 @@
 from django.db import models
-# from django.
+from core.models import BaseModel, User
+
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    ...
+
+
+class Address(BaseModel):
+    customer = models.ForeignKey(to=Customer, on_delete=models.SET_NULL)
+    address = models.TextField()
+    ...
